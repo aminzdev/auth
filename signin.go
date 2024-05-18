fab8daa4767cb1dad788654b8181d0fcb4e50ece
aminzdev <amin.zdev@gmail.com>
2024-05-18 16:13:35 +0330

    added signin method

---
+++ signin.go
@@ -0,0 +1,35 @@
+package auth
+
+import (
+	"errors"
+	"fmt"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"os"
+	"time"
+)
+
+func (a *Auth) SignIn(name, code string, duration time.Duration) error {
+	var user *User
+	if res := a.db.First(&user, "name = ?", name); res.Error != nil {
+		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			return ErrWrongCredentials
+		}
+		fmt.Fprintf(os.Stderr, "could not find user: %s", res.Error)
+		return ErrInternal
+	}
+
+	if user.Code != code {
+		return ErrWrongCredentials
+	}
+
+	res := a.db.
+		Clauses(clause.OnConflict{Columns: []clause.Column{{Name: "name"}}, UpdateAll: true}).
+		Create(&Session{Name: name, ExpiredAt: time.Now().Add(duration)})
+	if res.Error != nil {
+		fmt.Fprintf(os.Stderr, "could not create session: %s", res.Error)
+		return ErrInternal
+	}
+
+	return nil
+}
